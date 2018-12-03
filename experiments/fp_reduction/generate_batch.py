fix fp-reduction wrong axis problem. Use resnet3d model for fp-reduction

experiments/fp_reduction/generate_batch.py
@@ -3,6 +3,7 @@ import sys
 import pylidc as pl
 from tqdm import tqdm
 import numpy as np
+from pathlib import Path
 import pickle
 import random
 from sklearn.utils import shuffle
@@ -17,7 +18,11 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 sys.path.append('../')
+sys.path.append('/home/toosyou/projects/keras-retinanet/')
 from preprocessing import scan_index_split
+from keras_retinanet import models
+from scan_evaluate import ScanGenerator
+from keras_retinanet.utils.eval import _get_detections as get_fast_detection
 
 sys.path.append('/home/toosyou/projects/LungTumor')
 import data_util
@@ -94,39 +99,25 @@ def random_flip(patch):
             patch = np.flip(patch, i)
     return patch
 
+def extract_patch(volume, mask, x, y, z, patch_size=(64, 64, 16), do_random_flip=True):
+    patch = volume[ x-patch_size[0]//2 : x+patch_size[0]//2,
+                    y-patch_size[1]//2 : y+patch_size[1]//2,
+                    z-patch_size[2]//2 : z+patch_size[2]//2 ]
+
+    label = mask[ x-patch_size[0]//4 : x+patch_size[0]//4,
+                    y-patch_size[1]//4 : y+patch_size[1]//4,
+                    z-patch_size[2]//4 : z+patch_size[2]//4].any()
+
+    if patch.shape != patch_size:
+        return None, None
+
+    if do_random_flip:
+        patch = random_flip(patch)
+
+    patch = patch[..., np.newaxis]
+    return patch, label
+
 def get_patches(volume, size, is_positive, lung_mask, nodule_mask, layer_probability, patch_size=(64, 64, 16)):
-    def extract_patch(volume, mask, x, y, z, patch_size=patch_size):
-        """
-        xs = np.arange(-patch_size[0]//2, patch_size[0]//2, dtype=np.int) + x
-        ys = np.arange(-patch_size[1]//2, patch_size[1]//2, dtype=np.int) + y
-        zs = np.arange(-patch_size[2]//2, patch_size[2]//2, dtype=np.int) + z
-
-        patch = volume.take( xs, mode='wrap', axis=0).take(
-                            ys, mode='wrap', axis=1).take(
-                            zs, mode='warp', axis=2)
-
-        # center
-        xs = np.arange(-patch_size[0]//4, patch_size[0]//4, dtype=np.int) + x
-        ys = np.arange(-patch_size[1]//4, patch_size[1]//4, dtype=np.int) + y
-        zs = np.arange(-patch_size[2]//4, patch_size[2]//4, dtype=np.int) + z
-
-        label = mask.take( xs, mode='wrap', axis=0).take(
-                            ys, mode='wrap', axis=1).take(
-                            zs, mode='warp', axis=2).any()
-        """
-        patch = volume[ x-patch_size[0]//2 : x+patch_size[0]//2,
-                        y-patch_size[1]//2 : y+patch_size[1]//2,
-                        z-patch_size[2]//2 : z+patch_size[2]//2 ]
-
-        label = mask[ x-patch_size[0]//4 : x+patch_size[0]//4,
-                        y-patch_size[1]//4 : y+patch_size[1]//4,
-                        z-patch_size[2]//4 : z+patch_size[2]//4].any()
-
-        if patch.shape != patch_size:
-            return None, None
-
-        patch = random_flip(patch)[..., np.newaxis]
-        return patch, label
 
     # randomly choose one layer
     mask = nodule_mask.astype(np.bool) if is_positive else lung_mask.astype(np.bool)
@@ -228,17 +219,61 @@ def generate_positive_batch(set):
     np.save(filename, X)
     return None
 
+def generate_negative_detections(set, model_path):
+    os.makedirs(LIDC_IDRI_BATCHES_PREFIX, exist_ok=True)
+    os.makedirs(os.path.join(LIDC_IDRI_BATCHES_PREFIX, set), exist_ok=True)
+
+    indexes = scan_index_split(1018)[{'train': 0, 'valid': 1, 'test': 2}[set]]
+
+    # load fast-detection model to generate false positive patches
+    print('Loading model, this may take a second...')
+    fast_detection_model = models.load_model(
+                            model_path,
+                            'p3d',
+                            nms=True,
+                            convert=True)
+
+    all_detections = dict()
+    for index_scan in tqdm(indexes, desc='generate_negative_detections'):
+        scan_detections = list()
+        # load scan
+        volume, lung_mask, nodule_mask, layer_probability = get_scan(index_scan)
+        if volume is None:
+            continue
+
+        fast_detection_generator = ScanGenerator(volume)
+        fast_detections = get_fast_detection(
+                            generator=fast_detection_generator,
+                            model=fast_detection_model,
+                            score_threshold=0.05,
+                            max_detections=30,
+                            verbose=True,
+                            save_path=None,
+                            do_draw_annotations=False)
+
+        for z in np.arange(len(fast_detections)) + 8:
+            for d in fast_detections[z-8][0]:
+                scan_detections.append([d[0], d[1], d[2] ,d[3], z, d[4]])
+        all_detections[index_scan] = scan_detections
+
+    filename = os.path.join(LIDC_IDRI_BATCHES_PREFIX, set, 'all_detections.pl')
+    with open(filename, 'wb') as out_pl:
+        pickle.dump(all_detections, out_pl)
+    return None
+
 if __name__ == '__main__':
-    volume, lung_mask, nodule_mask, layer_probability = get_scan(0)
-    print(volume.shape)
-    cProfile.run("""get_patches(volume=volume,
-                    size=1024,
-                    is_positive=False,
-                    lung_mask=lung_mask,
-                    nodule_mask=nodule_mask,
-                    layer_probability=layer_probability,
-                    patch_size=(64, 64, 16))
-    """)
+    generate_negative_detections('train', '/home/toosyou/projects/keras-retinanet/experiments/working_models/augmented_smallanchor64.h5')
+    generate_negative_detections('valid', '/home/toosyou/projects/keras-retinanet/experiments/working_models/augmented_smallanchor64.h5')
+    # volume, lung_mask, nodule_mask, layer_probability = get_scan(0)
+    # print(volume.shape)
+    # cProfile.run("""get_patches(volume=volume,
+    #                 size=1024,
+    #                 is_positive=False,
+    #                 lung_mask=lung_mask,
+    #                 nodule_mask=nodule_mask,
+    #                 layer_probability=layer_probability,
+    #                 patch_size=(64, 64, 16))
+    # """)
     # generate_positive_batch('train')
     # generate_positive_batch('valid')
     # train, valid, test = scan_index_split(1018)

experiments/fp_reduction/unet_data_util.py
@@ -13,10 +13,13 @@ import time
 import random
 from tqdm import tqdm
 import pickle
+import cProfile
 
 import unet_3d
 from model import get_model, get_unet_model
 import datetime
+from resnet3d import Resnet3DBuilder
+from keras.optimizers import Adam
 
 import multiprocessing as mp
 import ctypes
@@ -33,11 +36,55 @@ sys.path.append('../..')
 sys.path.append('/home/toosyou/projects/LungTumor')
 import data_util
 from preprocessing import scan_index_split
-from generate_batch import get_scan, get_patches
+from generate_batch import get_scan, get_patches, extract_patch
 from keras_retinanet.callbacks import RedirectModel
 
 LIDC_IDRI_NP_PREFIX = '/mnt/ext/lidc_idri_np'
 LIDC_IDRI_BATCHES_PREFIX = '/mnt/ext3/lidc_idri_batches'
+
+def batch_generatorV3(set, batch_size, negative_ratio=0.9, n_batch_per_scan=10, n_scan_bundle=5):
+    # load positive patches
+    positive_patches = np.load(os.path.join(LIDC_IDRI_BATCHES_PREFIX, set, 'positive.npy'), mmap_mode='r')
+    # load all detections from fast_detection_model
+    all_detections = pickle.load(open(os.path.join(LIDC_IDRI_BATCHES_PREFIX, set, 'all_detections.pl'), 'rb'))
+    indexes = scan_index_split(1018)[{'train': 0, 'valid': 1, 'test': 2}[set]]
+
+    while True:
+        # load random scan
+        negative_Xs, negative_ys = list(), list()
+        for i in range(n_scan_bundle):
+            index_scan = np.random.choice(indexes)
+            volume, lung_mask, nodule_mask, layer_probability = get_scan(index_scan)
+            if volume is None:
+                continue
+
+            for index_detection in np.random.randint(len(all_detections[index_scan]), size=int(n_batch_per_scan*batch_size*negative_ratio)):
+                d = all_detections[index_scan][index_detection] # [x0, y0, x1, y1, z, score]
+                x, y, z = int((d[0]+d[2])/2), int((d[1]+d[3])/2), int(d[4])
+                patch, label = extract_patch(volume, nodule_mask, x, y, z)
+                if patch is None:
+                    continue
+
+                # normalize
+                negative_Xs.append(patch)
+                negative_ys.append([label, 1-label])
+
+        negative_Xs, negative_ys = np.array(negative_Xs), np.array(negative_ys)
+
+        for i in range(n_batch_per_scan*n_scan_bundle):
+            # randomly choose positive patches
+            positive_X = positive_patches[np.random.randint(positive_patches.shape[0], size=int(batch_size*(1.-negative_ratio))), ...]
+            positive_y = np.array([[1, 0]]*positive_X.shape[0])
+
+            negative_indexes = np.random.randint(negative_Xs.shape[0], size=int(batch_size*negative_ratio))
+            negative_X = negative_Xs[negative_indexes, ...]
+            negative_y = negative_ys[negative_indexes, ...]
+
+            # generate batch
+            X = np.append(negative_X, positive_X, axis=0)
+            y = np.append(negative_y, positive_y, axis=0)
+            X = (X - 418.) / 414. # normalize
+            yield shuffle(X, y)
 
 def batch_generatorV2(set, batch_size, negative_ratio=0.9, n_batch_per_scan=10, n_scan_bundle=5):
     # load positive patches
@@ -101,7 +148,10 @@ def batch_generator(set, batch_size):
 
 if __name__ == '__main__':
     # model, training_model = get_unet_model()
-    model, training_model = get_model()
+    # model, training_model = get_model()
+    model = Resnet3DBuilder.build_resnet_50((64, 64, 16, 1), 2)
+    training_model = keras.utils.multi_gpu_model(model)
+    training_model.compile(optimizer=Adam(amsgrad=True), loss='binary_crossentropy', metrics=['accuracy'])
     model.summary()
 
     callbacks=[
@@ -122,8 +172,8 @@ if __name__ == '__main__':
         # )
     ]
 
-    train_generator = batch_generatorV2('train', 128, n_batch_per_scan=20)
-    valid_generator = batch_generatorV2('valid', 128, n_batch_per_scan=20)
+    train_generator = batch_generatorV2('train', 128, n_batch_per_scan=20, negative_ratio=0.8)
+    valid_generator = batch_generatorV2('valid', 128, n_batch_per_scan=20, negative_ratio=0.8)
     training_model.fit_generator(train_generator,
                         steps_per_epoch=1024,
                         epochs=100,

experiments/scan_evaluate.py
@@ -4,6 +4,8 @@ import numpy as np
 import keras
 import tensorflow as tf
 from tqdm import tqdm
+import keras
+from keras import Model
 from keras.backend.tensorflow_backend import set_session
 from preprocessing import scan_index_split
 from keras.models import load_model
@@ -20,7 +22,8 @@ FAST_DETECTION_PARM = {
     'convert_model': True
 }
 
-FPR_MODEL_PATH = 'fp_reduction/working_models/conv3d_amsgrad_rop_v2_098.h5'
+# FPR_MODEL_PATH = 'fp_reduction/working_models/v3_conv3d_amsgrad_099.h5'
+FPR_MODEL_PATH = 'fp_reduction/model_checkpoints/95.h5'
 
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
@@ -32,9 +35,9 @@ def generate_fpr_batch(volume, boxes, z):
         ys = np.arange(-patch_size[1]//2, patch_size[1]//2, dtype=np.int) + y
         zs = np.arange(-patch_size[2]//2, patch_size[2]//2, dtype=np.int) + z
 
-        patch = volume.take( xs, mode='wrap', axis=0).take(
-                            ys, mode='wrap', axis=1).take(
-                            zs, mode='warp', axis=2)
+        patch = volume.take( ys, mode='wrap', axis=0).take(
+                                xs, mode='wrap', axis=1).take(
+                                zs, mode='warp', axis=2)
         return patch
 
     batch = list()
@@ -99,11 +102,11 @@ def predict(set, index):
     fast_detection_model = models.load_model(
                             FAST_DETECTION_PARM['path'],
                             backbone_name=FAST_DETECTION_PARM['backbone'],
+                            nms=True,
                             convert=FAST_DETECTION_PARM['convert_model'])
     # fast_detection_model.summary()
 
     fpr_model = load_model(FPR_MODEL_PATH)
-    # fpr_model.summary()
 
     fpr_model = keras.utils.multi_gpu_model(fpr_model)
 
@@ -112,7 +115,7 @@ def predict(set, index):
                             generator=fast_detection_generator,
                             model=fast_detection_model,
                             score_threshold=0.05,
-                            max_detections=60,
+                            max_detections=30,
                             verbose=True,
                             save_path=None,
                             do_draw_annotations=False)

experiments/utils.py
@@ -60,20 +60,7 @@ def get_dicom_volume(folder_path):
 
     return images2volume(images) # convert images to volume
 
-def get_patches(scan, z_patch=16, verbose=False):
-    def get_max_bboxes(nods):
-        max_bboxes = list()
-        for nod in nods:
-            max_bbox = [np.Inf, -np.Inf, np.Inf, -np.Inf, np.Inf, -np.Inf]
-            for annotation in nod:
-                bbox = annotation.bbox()
-                # update max_bbox
-                for i in range(3):
-                    max_bbox[i*2] = min(max_bbox[i*2], int(bbox[i].start))
-                    max_bbox[i*2+1] = max(max_bbox[i*2+1], int(bbox[i].stop))
-            max_bboxes.append(max_bbox)
-        return max_bboxes
-
+def get_patches(index_scan, include_negative=False, randomly_select=False, z_patch=16, verbose=False):
     def bboxes_contain_z(z, bboxes):
         selected = list()
         for bbox in bboxes:
@@ -88,15 +75,32 @@ def get_patches(scan, z_patch=16, verbose=False):
         return labels
 
     X, y = list(), list()
-    volume = scan.to_volume(verbose=False)
+    if os.path.isfile(os.path.join('/mnt/ext/lidc_idri_np', str(index_scan), 'volume.npy')):
+        if randomly_select:
+            volume = np.load(os.path.join('/mnt/ext/lidc_idri_np', str(index_scan), 'volume.npy'), mmap_mode='r')
+        else:
+            volume = np.load(os.path.join('/mnt/ext/lidc_idri_np', str(index_scan), 'volume.npy'))
+        bboxes = np.load(os.path.join('/mnt/ext/lidc_idri_np', str(index_scan), 'bboxes.npy'))
+    else:
+        return None, None
     # lung_mask = data_util.lung_mask(volume, times_dilation=20, times_erosion=15, verbose=verbose)
     # volume[lung_mask < 0.5] = volume.min()
-    nods = scan.cluster_annotations()
-    bboxes = get_max_bboxes(nods)
 
-    for z in range(z_patch//2, volume.shape[2]-z_patch//2):
+    if randomly_select:
+        if include_negative:
+            zs = [np.random.randint(z_patch//2, volume.shape[2]-z_patch//2)]
+        else:
+            positive_zs = list()
+            for z in range(z_patch//2, volume.shape[2]-z_patch//2):
+                selected_bboxes = bboxes_contain_z(z, bboxes)
+                if len(selected_bboxes): positive_zs.append(z)
+            zs = [np.random.choice(positive_zs)]
+    else:
+        zs = list(range(z_patch//2, volume.shape[2]-z_patch//2))
+
+    for z in zs:
         selected_bboxes = bboxes_contain_z(z, bboxes)
-        if len(selected_bboxes):
+        if include_negative or len(selected_bboxes):
             X.append(volume[:,:, z-z_patch//2: z+z_patch//2])
             y.append(generate_labels(selected_bboxes))
 